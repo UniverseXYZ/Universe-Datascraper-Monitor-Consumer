d26cf62f8dbd869f47ebb03f8084fe3c2c698945
Ryan Shtirmer <rshtirmer@gmail.com>
2022-03-24 10:29:14 -0400

    Use chainstack

--- src/modules/ethereum/ethereum.service.ts
+++ src/modules/ethereum/ethereum.service.ts
@@ -17,52 +17,21 @@ const ERC165ABI = [
 ];
 
 @Injectable()
-export class EthereumService {
-  public ether: ethers.providers.BaseProvider;
-  private readonly logger = new Logger(EthereumService.name);
-
   constructor(private configService: ConfigService) {
     const network: ethers.providers.Networkish = this.configService.get('ethereum_network');
-    const quorum: number = Number(this.configService.get('ethereum_quorum'));
-
-    const projectSecret: string = this.configService.get('infura.project_secret');
-    const projectId: string = this.configService.get('infura.project_id');
-    const infuraProvider: ethers.providers.InfuraProvider = projectId && projectSecret
-      ? new ethers.providers.InfuraProvider(network, {
-          projectId: projectId,
-          projectSecret: projectSecret
-        })
-      : undefined;
-        
-    const alchemyToken: string = this.configService.get('alchemy_token')
-    const alchemyProvider: ethers.providers.AlchemyProvider = alchemyToken
-      ? new ethers.providers.AlchemyProvider(network, alchemyToken)
-      : undefined;
 
     const chainstackUrl: string = this.configService.get('chainstack_url')
     const chainStackProvider: ethers.providers.JsonRpcProvider = chainstackUrl
       ? new ethers.providers.JsonRpcProvider(chainstackUrl, network)
       : undefined;
 
-    const quicknodeUrl: string = this.configService.get('quicknode_url');
-    const quicknodeProvider: ethers.providers.JsonRpcProvider = quicknodeUrl
-      ? new ethers.providers.JsonRpcProvider(quicknodeUrl, network)
-      : undefined;
-
-
-    if (!infuraProvider && !alchemyProvider && !chainStackProvider && !quicknodeProvider) {
+    if (!chainStackProvider) {
       throw new Error(
-        'Infura project id and secret or alchemy token or chainstack url is not defined',
+        'Chainstack url is not defined',
       );
     }
-    
-    const allProviders: ethers.providers.BaseProvider[] = [infuraProvider, alchemyProvider, chainStackProvider, quicknodeProvider]
-    const definedProviders: ethers.providers.BaseProvider[] = allProviders.filter(x => x !== undefined);
-
-    this.logger.log(`[Monitor Consumer]: Creating FBProvider w/ ${definedProviders.length} providers`)
-    
-    const ethersProvider: ethers.providers.FallbackProvider = new ethers.providers.FallbackProvider(definedProviders, quorum);
-    this.ether = ethersProvider;
+        
+    this.ether = chainStackProvider;
   }
 
   async getContractsInBlock(allAddress: string[], blockNum: number) {
